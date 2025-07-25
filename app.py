2df30557e2e879132c9e3b436282a002569aee69
Victore Bouvie <victore.bouvie.dev@gmail.com>
2025-07-25 20:37:28 -0300

    feat: Create /api/projects endpoint to serve project data

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -5,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def get_home():
     return jsonify({"message": "Api no ar!"})
+
+@app.route("/api/projects")
+def get_projects():
+    with open('projects.json', 'r', encoding='utf-8') as file:
+        projects_data = json.load(file)
+    return jsonify(projects_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
