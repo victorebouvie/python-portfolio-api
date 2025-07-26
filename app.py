ff3e12c93ee1194a6724be6a903253ec54f96342
Victore Bouvie <victore.bouvie.dev@gmail.com>
2025-07-25 21:42:36 -0300

    fix: implement CORS to allow cross-origin requests and Att on requirements

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import json
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+CORS(app)
 @app.route("/")
 def get_home():
     return jsonify({"message": "Api no ar!"})
